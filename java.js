f99692563264841b6c766d5c8e171ae3b2d5d008
alc-yk <125387583+alc-yk@users.noreply.github.com>
2023-07-11 14:55:44 -0500

    Begin function creation for computer choice

---
+++ java.js
@@ -0,0 +1,18 @@
+let playerSelection = prompt("Enter choice", "rock, paper, or scissors");
+const computerSelection = getComputerChoice();
+
+const choices = ["rock", "paper", "scissors"];
+
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let choice = Math.random(choices);
+    return choice;
+}
+
+function playRound (playerSelection, computerSelection) {
+
+
+
+}
+
