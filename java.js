27b4d7dbeb9c79c1adc0451ba8c886cf5eba8ed1
alc-yk <125387583+alc-yk@users.noreply.github.com>
2023-07-18 14:53:52 -0500

    Added if statements for RPS situations

--- java.js
+++ java.js
@@ -5,13 +5,32 @@ function getComputerChoice(arr) {
 }
 
 function playRound (playerSelection, computerSelection) {
-
-
-
+    if (playerSelection == 'rock' && computerSelection == 'paper') {
+        print('YOU BIG LOSER, PAPER BEATS ROCK');
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        print('WINNER WINNER! ROCK DOES BEAT SCISSORS');
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        print('YOU BIG LOSER, SCISSORS BEATS PAPER');
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        print('WINNER WINNER! PAPER DOES BEAT ROCK');
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        print('YOU BIG LOSER, ROCK BEATS SCISSORS');
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        print('WINNER WINNER! SCISSORS DOES BEAT PAPER');
+    }
+    else {
+        print('TIE GO AGAIN!')
+    }
 }
 
 const choices = ['rock', "paper", "scissors"];
 let playerSelection = prompt("Enter choice", "rock, paper, or scissors");
 const computerSelection = getComputerChoice(choices);
 
-console.log(computerSelection)
+
+console.log(playRound(playerSelection, computerSelection));
