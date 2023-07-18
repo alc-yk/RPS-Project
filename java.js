d207e66ac97d7b570823b0687eef6fc1174e1fb6
alc-yk <125387583+alc-yk@users.noreply.github.com>
2023-07-18 14:27:07 -0500

    Working computer choice function

--- java.js
+++ java.js
@@ -1,13 +1,7 @@
-let playerSelection = prompt("Enter choice", "rock, paper, or scissors");
-const computerSelection = getComputerChoice();
 
-const choices = ["rock", "paper", "scissors"];
-
-
-function getComputerChoice() {
-    const choices = ["rock", "paper", "scissors"];
-    let choice = Math.random(choices);
-    return choice;
+function getComputerChoice(arr) {
+    let choice = Math.floor(Math.random(choices) * arr.length); //math.random selects a number between 0-choices.length -- math.floor returns nearest int
+    return arr[choice]; // receives the item from array using random int generated 
 }
 
 function playRound (playerSelection, computerSelection) {
@@ -16,3 +10,8 @@ function playRound (playerSelection, computerSelection) {
 
 }
 
+const choices = ['rock', "paper", "scissors"];
+let playerSelection = prompt("Enter choice", "rock, paper, or scissors");
+const computerSelection = getComputerChoice(choices);
+
+console.log(computerSelection)
